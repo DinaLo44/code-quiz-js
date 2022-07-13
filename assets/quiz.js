d94e3ddf9fd3dffa534ddc9589d3370d65830292
DinaLo <dinalo.brady@gmail.com>
2022-07-12 22:00:10 -0600

    deleted buttons in HTML and CSS, and created functions in JS

--- assets/quiz.js
+++ assets/quiz.js
@@ -1,54 +1,41 @@
+var interval;
 var question = document.querySelector('#question');
-var options = Array.from(document.querySelector('.options-text'));
 var score = document.querySelector('#score-counter');
+var answersEl = document.getElementById('answers');
+var timerDisplay = document.getElementById('timer-display');
 
 var currentQuestions = {};
 var correctAnswers = true;
-var score = 0;
+var timeLeft = 60;
 var questionCounter = 0;
 var allQuestions = [];
-
+var finalScores = JSON.parse(localStorage.getItem('finalScores')) || []
 var questions = [
-{
-    question: 'What is the keyword used to pop up a message to a user?',
-    option1: 'message',
-    option2: 'alert',
-    option3: 'input',
-    option4: 'text',
-    answer: 2,
-},
-{
-    question: 'What kind of value can you assign to a variable?',
-    option1: 'string',
-    option2: 'number',
-    option3: 'boolean',
-    option4: 'all of the above',
-    answer: 4,
-},
-{
-    question: 'What is the semicolon used for?',
-    option1: 'To start a statement',
-    option2: 'To replace a parenthesis',
-    option3: 'to replace a curly bracket',
-    option4: 'To end a statement',
-    answer: 4,
-},
-{
-    question: 'What is the name of the method that adds elements to the end of an array?',
-    option1: 'push',
-    option2: 'shift',
-    option3: 'pop',
-    option4: 'splice',
-    answer: 1,
-},
-{
-    question: 'These are examples of comparison operators:',
-    option1: '+, -, /, *',
-    option2: '%, #, &&, ||',
-    option3: '!==, ===, >, >=',
-    option4: '>, ?, *, =',
-    answer: 3,
-},
+    {
+        question: 'What is the keyword used to pop up a message to a user?',
+        option: ['message', 'alert', 'input', 'text'],
+        answer: 'alert',
+    },
+    {
+        question: 'What kind of value can you assign to a variable?',
+        option: ['string', 'number', 'boolean', 'all of the above'],
+        answer: 'all of the above',
+    },
+    {
+        question: 'What is the semicolon used for?',
+        option: ['To start a statement', 'To replace a parenthesis', 'To replace a curly bracket', 'To end a statement'],
+        answer: 'To end a statement',
+    },
+    {
+        question: 'What is the name of the method that adds elements to the end of an array?',
+        option: ['push', 'shift', 'pop', 'splice'],
+        answer: 'push',
+    },
+    {
+        question: 'These are examples of comparison operators:',
+        option: ['+, -, /, *', '%, #, &&, ||', '!==, ===, >, >=', '>, ?, *, ='],
+        answer: '!==, ===, >, >=',
+    },
 ]
 
 var totalScoredPoints = 100;
@@ -57,67 +44,95 @@ var maxNumQuestions = 5;
 function startQuiz() {
     score = 0;
     questionCounter = 0;
-    allQuestions = [...questions];
     getNewQuestion();
-} 
-
-function getNewQuestion () {
-    if (allQuestions.length === 0 || questionCounter > maxNumQuestions) {
-       localStorage.storage.setItem('mostRecentScore', score) 
-
-       return window.location.assign('/.conclusion.html')
-    }
-
-    questionCounter++
-
-    questionsInit = Math.floor(Math.random() * allQuestions.length)
-    currentQuestions = allQuestions[questionsInit]
-    questions.innerText = currentQuestions.questions
-
-    options.forEach(option => {
-        var numberOptions = options.dataset['number']
-        option.innerText = currentQuestions['option' + numberOptions]
-    })
-    allQuestions.splice(questionInit, 1)
-
-    correctAnswers = true
+    startTimer()
 }
 
-options.forEach(option => {
-    option.addEventListener('click', e => {
-        if (!correctAnswers) return
+function getNewQuestion() {
 
-        correctAnswers = false
-        var selectedOption = e.target
-        var selectedAnswer = selectedOption.dataset['number']
+    var currentQuestion = questions[questionCounter]
+    question.innerText = currentQuestion.question
 
-        let classToApply = selectedAnswer == currentQuestions.answer ? 'correct' : 'incorrect'
+    var aLabel = ['A', 'B', 'C', 'D']
+    answersEl.innerHTML = ''
+    for (var i = 0; i < currentQuestion.option.length; i++) {
+        var div = document.createElement('div')
+        div.classList.add('options-container')
+        var pLabel = document.createElement('p')
+        pLabel.classList.add('options-prefix')
+        pLabel.textContent = aLabel[i]
+        pAnswers = document.createElement('p')
+        pAnswers.classList.add('options-text')
+        pAnswers.textContent = currentQuestion.option[i]
+        div.append(pLabel, pAnswers)
+        answersEl.append(div)
+    }
 
-        if (classToApply === 'correct') {
-            incrementScore(totalScoredPoints)
+}
+
+function submitFormHandler(event) {
+    var inputEl = document.getElementsByTagName('input')[0]
+    console.log(inputEl.value)
+    var tempObject = {
+        user: inputEl.value,
+        score: timeLeft,
+    }
+    finalScores.push(tempObject)
+    localStorage.setItem('finalScores', JSON.stringify(finalScores))
+}
+
+function answerHandler(event) {
+    if (event.target.matches('button')) {
+        submitFormHandler(event)
+    }
+    if (event.target.matches('p') && event.target.classList.contains('options-text')) {
+        var currentQuestion = questions[questionCounter]
+
+        if (currentQuestion.answer === event.target.textContent) {
+            console.log('true')
+        }
+        else {
+            console.log('false')
+            timeLeft = timeLeft - 10
         }
 
-        selectedOption.parenElement.classList.add(classToApply)
-//Reference for setting up the Timer: https://stackoverflow.com/questions/58964755/subtract-time-from-timer-if-answer-is-wrong-creating-a-quiz-javascript
-        function startTimer() {
-            console.log('Timer starts')
-            var seconds = 59;
-            var timer = setInterval(function () {
-                document.getElementById('timer-display').innerHTML='00:' + seconds;
-                seconds--;
-                if (seconds < 0) {
-                    clearInterval(timer);
-                    alert("Time is up!")
-                }
-            }, 1000);
-        }
-        incrementScore = number => {
-            score-counter >= number
-            score-counter.innerText === score
+        questionCounter++
+        
+        //If else for either ending the game or bringing new question
+        if (questionCounter === questions.length) {
+            endQuiz();
+        } else {
+            getNewQuestion()
         }
 
-        startQuiz()
+    }
+}
 
+function endQuiz() {
+    clearInterval(interval)
+    answersEl.innerHTML = ''
+    question.textContent = 'Please submit your initials'
+    creatingInitials()
+}
 
-    })
-})
+function creatingInitials() {
+    var inputEl = document.createElement('input')
+    inputEl.setAttribute('type', 'text')
+    var submitBtn = document.createElement('button')
+    submitBtn.textContent = 'Submit'
+    submitBtn.setAttribute('id', 'intialsBtn')
+    answersEl.append(inputEl, submitBtn)
+}
+
+function startTimer() {
+    interval = setInterval(function () {
+        timeLeft--
+        timerDisplay.textContent = timeLeft
+        if (timeLeft <= 0) {
+            endQuiz()
+        }
+    }, 1000)
+}
+
+answersEl.addEventListener('click', answerHandler)
+startQuiz()
